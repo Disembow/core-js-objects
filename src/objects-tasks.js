7ffb28999c6adfee289ae88536f0fd38bd3940f4
Disembow <e_naletov@mail.ru>
2024-02-24 14:00:37 +0100

    feat: implement 8th task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -170,8 +170,17 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const ticket = 25;
+  let change = 0;
+  let canSell = true;
+
+  queue.forEach((sum) => {
+    if (sum <= ticket) change += sum;
+    else if (sum - change > 0) canSell = false;
+  });
+
+  return canSell;
 }
 
 /**
